15e10342b116bb8168d11227cc0c97e0a8e43331
Samuel Torres <est.samuelj.torres@unimilitar.edu.co>
2026-04-24 09:38:38 -0500

    implementacion de las funciones para pixel, bresenham y la logica dentro de este.

---
+++ script.js
@@ -0,0 +1,50 @@
+/**
+ * Universidad - Facultad de Ingeniería
+ * Asignatura: Introducción a la Computación Gráfica
+ * Tarea: Rasterización manual (Bresenham + Punto Medio)
+ */
+
+//  funcion para PIXEL 
+function drawPixel(ctx, x, y, color = "#000") {
+    ctx.fillStyle = color;
+    ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
+
+// la funcion Bresenham
+function bresenhamLine(ctx, x0, y0, x1, y1, color = "#000") {
+
+    x0 = Math.floor(x0);
+    y0 = Math.floor(y0);
+    x1 = Math.floor(x1);
+    y1 = Math.floor(y1);
+
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+
+    let sx = (x0 < x1) ? 1 : -1;
+    let sy = (y0 < y1) ? 1 : -1;
+
+    let err = dx - dy;
+
+    while (true) {
+        drawPixel(ctx, x0, y0, color);
+
+        if (x0 === x1 && y0 === y1) break;
+
+        let e2 = 2 * err;
+
+        /**Lógica del error:
+         * e2 evalúa si debemos movernos en X, Y o ambos.
+         * Permite manejar todos los octantes (m > 1, m < 0, etc).*/
+
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
